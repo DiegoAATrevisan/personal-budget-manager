e3fb8a9a3cc46dcdc56fc4425a5888b53e98bff3
DiegoAATrevisan <augustodiego02@gmail.com>
2024-09-18 17:20:09 -0300

    Adicionado o Contexto porem ainda sem funcionar corretamente.

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,13 @@ import { BrowserRouter as Router, Routes, Route, Link, } from 'react-router-dom'
 import Home from './pages/home';
 import Dashboard from './pages/dashborad';
 import Sobre from './pages/sobre';
+import { ContextDashboard } from './context/contextDashboard';
+import { DashboardService } from './utils/DashboardService';
 
 function App() {
+
+  const dashboardService = new DashboardService();
+
   const renderButtons = () => {
     return (
       <div>
@@ -23,7 +28,11 @@ function App() {
       {renderButtons()}
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard" element={
+          <ContextDashboard.Provider value={{ dashboardService }}>
+            <Dashboard />
+          </ContextDashboard.Provider>
+        } />
         <Route path="/sobre" element={<Sobre />} />
       </Routes>
     </Router>
